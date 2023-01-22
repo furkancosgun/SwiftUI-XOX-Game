f1f40eee084f325766981482c5dcde452a4f4b25
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-22 21:53:58 +0300

    Create xoxGameApp.swift

---
+++ xoxGame/xoxGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  xoxGameApp.swift
+//  xoxGame
+//
+//  Created by Furkan on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct xoxGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
